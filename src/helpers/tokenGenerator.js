4861db6bc92863524eaf3d955aefde989f8f2164
Boluwatife Fakorede <bfakorede@avallain.com>
2020-05-10 04:45:21 +0100

    Implement First session for company and basic crud activity

---
+++ src/helpers/tokenGenerator.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+const TOKEN_KEY = process.env.TOKEN_KEY
+
+export default function tokenGenerator({ hashInfo }) {
+  const token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), ...hashInfo }, TOKEN_KEY, { algorithm: 'HS256'});
+  return token;
+}
---
+++ src/middlewares/jwt-validator.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const TOKEN_KEY = process.env.TOKEN_KEY
+
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+      jwt.verify(authHeader, TOKEN_KEY, (err, user) => {
+          if (err) {
+              return res.sendStatus(403);
+          }
+
+          req.user = user;
+          next();
+      });
+  } else {
+      res.sendStatus(401);
+  }
+};
+
+export default authenticateJWT;
